b182aca086ba6e770a8214cd7c9066a0d77ed3c0
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2021-12-13 15:33:35 +0300

    implemented request status check

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from pathlib import Path
+from random import randint
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_status(response):
+    response.raise_for_status()
+    if response.json().get('error'):
+        raise requests.exceptions.HTTPError(response=response)
 
 
 # Press the green button in the gutter to run the script.

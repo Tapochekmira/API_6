9a8c70d94d02a95feb6b13f1c970376b7675ffda
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2021-12-13 15:34:10 +0300

    download random comic

--- main.py
+++ main.py
@@ -12,7 +12,31 @@ def check_status(response):
         raise requests.exceptions.HTTPError(response=response)
 
 
-# Press the green button in the gutter to run the script.
+def get_random_comics_number():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    current_comic_number = response.json()['num']
+    return randint(1, current_comic_number)
+
+
+def download_random_comic():
+    comic_number = get_random_comics_number()
+    comic_url = f'https://xkcd.com/{comic_number}/info.0.json'
+    response = requests.get(comic_url)
+    response.raise_for_status()
+
+    response = response.json()
+    comic_comment = response['alt']
+    download_comic_url = response['img']
+
+    response = requests.get(download_comic_url)
+    response.raise_for_status()
+    with open(f'{directory}comic.png', 'wb') as comic_file:
+        comic_file.write(response.content)
+
+    return comic_comment
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
